defd76b3cf366e74807dd8dc8911735846405d55
Mark65537 <55081239+Mark65537@users.noreply.github.com>
2022-10-14 14:46:47 +0700

    Добавьте файлы проекта.

---
+++ ColoredToMono/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace ColoredToMono
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cMS_fotPicBox = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TSMI_normal = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_stretchImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_CenterImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_zoom = new System.Windows.Forms.ToolStripMenuItem();
+            this.pB_mono = new System.Windows.Forms.PictureBox();
+            this.b_export = new System.Windows.Forms.Button();
+            this.l_fileName = new System.Windows.Forms.Label();
+            this.b_openImages = new System.Windows.Forms.Button();
+            this.oFD = new System.Windows.Forms.OpenFileDialog();
+            this.sFD = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.l_time = new System.Windows.Forms.Label();
+            this.b_next = new System.Windows.Forms.Button();
+            this.b_prev = new System.Windows.Forms.Button();
+            this.NUpD_lavel = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cMS_fotPicBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pB_mono)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUpD_lavel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cMS_fotPicBox
+            // 
+            this.cMS_fotPicBox.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMI_normal,
+            this.TSMI_stretchImage,
+            this.TSMI_CenterImage,
+            this.TSMI_zoom});
+            this.cMS_fotPicBox.Name = "cMS_fotPicBox";
+            this.cMS_fotPicBox.Size = new System.Drawing.Size(145, 92);
+            this.cMS_fotPicBox.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.cMS_fotPicBox_ItemClicked);
+            // 
+            // TSMI_normal
+            // 
+            this.TSMI_normal.Name = "TSMI_normal";
+            this.TSMI_normal.Size = new System.Drawing.Size(144, 22);
+            this.TSMI_normal.Text = "Normal";
+            // 
+            // TSMI_stretchImage
+            // 
+            this.TSMI_stretchImage.Name = "TSMI_stretchImage";
+            this.TSMI_stretchImage.Size = new System.Drawing.Size(144, 22);
+            this.TSMI_stretchImage.Text = "StretchImage";
+            // 
+            // TSMI_CenterImage
+            // 
+            this.TSMI_CenterImage.Name = "TSMI_CenterImage";
+            this.TSMI_CenterImage.Size = new System.Drawing.Size(144, 22);
+            this.TSMI_CenterImage.Text = "CenterImage";
+            // 
+            // TSMI_zoom
+            // 
+            this.TSMI_zoom.Name = "TSMI_zoom";
+            this.TSMI_zoom.Size = new System.Drawing.Size(144, 22);
+            this.TSMI_zoom.Text = "Zoom";
+            // 
+            // pB_mono
+            // 
+            this.pB_mono.BackColor = System.Drawing.Color.DarkGray;
+            this.pB_mono.ContextMenuStrip = this.cMS_fotPicBox;
+            this.pB_mono.Location = new System.Drawing.Point(12, 40);
+            this.pB_mono.Name = "pB_mono";
+            this.pB_mono.Size = new System.Drawing.Size(400, 240);
+            this.pB_mono.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pB_mono.TabIndex = 0;
+            this.pB_mono.TabStop = false;
+            this.pB_mono.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pB_mono_MouseClick);
+            // 
+            // b_export
+            // 
+            this.b_export.Location = new System.Drawing.Point(293, 11);
+            this.b_export.Name = "b_export";
+            this.b_export.Size = new System.Drawing.Size(75, 23);
+            this.b_export.TabIndex = 2;
+            this.b_export.Text = "Export";
+            this.b_export.UseVisualStyleBackColor = true;
+            this.b_export.Click += new System.EventHandler(this.b_export_Click);
+            // 
+            // l_fileName
+            // 
+            this.l_fileName.AutoEllipsis = true;
+            this.l_fileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_fileName.Location = new System.Drawing.Point(76, 295);
+            this.l_fileName.Name = "l_fileName";
+            this.l_fileName.Size = new System.Drawing.Size(266, 20);
+            this.l_fileName.TabIndex = 3;
+            this.l_fileName.Text = "..";
+            this.l_fileName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // b_openImages
+            // 
+            this.b_openImages.Location = new System.Drawing.Point(189, 11);
+            this.b_openImages.Name = "b_openImages";
+            this.b_openImages.Size = new System.Drawing.Size(81, 23);
+            this.b_openImages.TabIndex = 4;
+            this.b_openImages.Text = "Open";
+            this.b_openImages.UseVisualStyleBackColor = true;
+            this.b_openImages.Click += new System.EventHandler(this.b_openImages_Click);
+            // 
+            // oFD
+            // 
+            this.oFD.FileName = "test";
+            this.oFD.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+            this.oFD.Multiselect = true;
+            // 
+            // sFD
+            // 
+            this.sFD.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image " +
+    "Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            this.sFD.Title = "Сохранить картинку как...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(374, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Time:";
+            // 
+            // l_time
+            // 
+            this.l_time.AutoSize = true;
+            this.l_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_time.Location = new System.Drawing.Point(419, 12);
+            this.l_time.Name = "l_time";
+            this.l_time.Size = new System.Drawing.Size(21, 20);
+            this.l_time.TabIndex = 6;
+            this.l_time.Text = "...";
+            // 
+            // b_next
+            // 
+            this.b_next.Enabled = false;
+            this.b_next.Location = new System.Drawing.Point(363, 292);
+            this.b_next.Name = "b_next";
+            this.b_next.Size = new System.Drawing.Size(49, 23);
+            this.b_next.TabIndex = 7;
+            this.b_next.Text = "→";
+            this.b_next.UseVisualStyleBackColor = true;
+            this.b_next.Click += new System.EventHandler(this.b_next_Click);
+            // 
+            // b_prev
+            // 
+            this.b_prev.Enabled = false;
+            this.b_prev.Location = new System.Drawing.Point(12, 295);
+            this.b_prev.Name = "b_prev";
+            this.b_prev.Size = new System.Drawing.Size(49, 23);
+            this.b_prev.TabIndex = 7;
+            this.b_prev.Text = "←";
+            this.b_prev.UseVisualStyleBackColor = true;
+            this.b_prev.Click += new System.EventHandler(this.b_prev_Click);
+            // 
+            // NUpD_lavel
+            // 
+            this.NUpD_lavel.Location = new System.Drawing.Point(44, 11);
+            this.NUpD_lavel.Maximum = new decimal(new int[] {
+            240,
+            0,
+            0,
+            0});
+            this.NUpD_lavel.Name = "NUpD_lavel";
+            this.NUpD_lavel.Size = new System.Drawing.Size(120, 20);
+            this.NUpD_lavel.TabIndex = 9;
+            this.NUpD_lavel.Value = new decimal(new int[] {
+            125,
+            0,
+            0,
+            0});
+            this.NUpD_lavel.ValueChanged += new System.EventHandler(this.NUpD_lavel_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "lavel";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 336);
+            this.Controls.Add(this.NUpD_lavel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.b_prev);
+            this.Controls.Add(this.b_next);
+            this.Controls.Add(this.l_time);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b_openImages);
+            this.Controls.Add(this.l_fileName);
+            this.Controls.Add(this.b_export);
+            this.Controls.Add(this.pB_mono);
+            this.Name = "Form1";
+            this.Text = "ColoredToMono";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cMS_fotPicBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pB_mono)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUpD_lavel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pB_mono;
+        private System.Windows.Forms.Button b_export;
+        private System.Windows.Forms.Label l_fileName;
+        private System.Windows.Forms.Button b_openImages;
+        private System.Windows.Forms.OpenFileDialog oFD;
+        private System.Windows.Forms.SaveFileDialog sFD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label l_time;
+        private System.Windows.Forms.Button b_next;
+        private System.Windows.Forms.Button b_prev;
+        private System.Windows.Forms.ContextMenuStrip cMS_fotPicBox;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_normal;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_stretchImage;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_CenterImage;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_zoom;
+        private System.Windows.Forms.NumericUpDown NUpD_lavel;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ColoredToMono/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColoredToMono
+{
+    public partial class Form1 : Form
+    {
+        delegate void Mes(); // 1. Объявляем делегат
+        Mes mes = ColoredToBW;            // 2. Создаем переменную делегата
+        //mes();                  // 4. Вызываем метод
+
+        string[] fileNames;
+        string[] safeFileNames;
+        uint fNCount=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+
+        private void ToGrayScale(Image img)
+        {
+            Bitmap Bmono;
+            using (Bitmap Bclr = new Bitmap(img))
+            {
+                Bmono = new Bitmap(Bclr.Width, Bclr.Height);
+                for (int y = 0; y < Bclr.Height; y++)
+                {
+                    for (int x = 0; x < Bclr.Width; x++)
+                    {
+                        Color p = Bclr.GetPixel(x, y);
+                        if (p != null)
+                        {
+                            int grayScale = (int)((p.R * .3) +
+                                 (p.G * .59) + (p.B * .11));
+
+                            //create the color object
+                            Color monoColor = Color.FromArgb(grayScale, grayScale, grayScale);
+                            Bmono.SetPixel(x, y, monoColor);
+                        }
+                    }
+                }
+            }
+            pB_mono.Image = Bmono;
+        }
+
+        private void ToGrayScale2(Image img)
+        {
+            Bitmap Bmono;
+            using (Bitmap Bclr = new Bitmap(img))
+            {
+                Bmono = new Bitmap(Bclr.Width, Bclr.Height);
+                for (int y = 0; y < Bclr.Height; y++)
+                {
+                    for (int x = 0; x < Bclr.Width; x++)
+                    {
+                        int pixel = Bclr.GetPixel(x, y).ToArgb();
+                        // получаем компоненты цветов пикселя
+                        float R = (pixel & 0x00FF0000) >> 16; // красный
+                        float G = (pixel & 0x0000FF00) >> 8; // зеленый
+                        float B = pixel & 0x000000FF; // синий
+                                                               
+                        R = G = B = (R + G + B) / 3.0f;// делаем цвет черно-белым (оттенки серого) - находим среднее арифметическое
+                        // собираем новый пиксель по частям (по каналам)
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+                        // добавляем его в Bitmap нового изображения
+                        Bmono.SetPixel(x, y, Color.FromArgb((int)newPixel));
+                    }
+                }
+            }
+            pB_mono.Image = Bmono;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text += Assembly.GetExecutingAssembly().GetName().Version;
+            MinimumSize = Size;
+        }
+
+        private void b_export_Click(object sender, EventArgs e)
+        {
+            if (pB_mono.Image != null)
+            {                
+                if (sFD.ShowDialog() == DialogResult.OK)// если в диалоге была нажата кнопка ОК
+                {
+                    try
+                    {
+                        pB_mono.Image.Save(sFD.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка при сохранении изображения", "Ошибка",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }                                       
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void b_openImages_Click(object sender, EventArgs e)
+        {
+            DialogResult dr=oFD.ShowDialog();           
+            if (dr==DialogResult.OK) // если в диалоге была нажата кнопка ОК
+            {
+                try
+                {
+                    fileNames=oFD.FileNames;
+                    safeFileNames = oFD.SafeFileNames;
+                    l_fileName.Text = safeFileNames[0];
+                    pB_mono.ImageLocation = fileNames[0];// загружаем изображение
+
+                    if (fileNames.Length>1)
+                    {
+                        b_next.Enabled = true;
+                    }
+
+                    
+
+                    Stopwatch stopwatch = new Stopwatch();//создаем объект для того что бы засеч время
+                    stopwatch.Start();//засекаем время начала операции
+
+                     ColoredToBW(fileNames[0]);
+
+                    stopwatch.Stop();//останавливаем счётчик            
+                    TimeSpan ts = stopwatch.Elapsed;
+
+                    string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", // Создаем строку, содержащую время выполнения операции.
+                        ts.Hours, ts.Minutes, ts.Seconds,
+                        ts.Milliseconds / 10);
+
+                    l_time.Text = elapsedTime;
+                }
+                catch // в случае ошибки выводим MessageBox
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }                
+            }
+        }
+
+        void stopWatchFunc(Delegate func)
+        {
+            Stopwatch stopwatch = new Stopwatch();//создаем объект для того что бы засеч время
+            stopwatch.Start();//засекаем время начала операции
+
+            func(fileNames[0]);
+
+            stopwatch.Stop();//останавливаем счётчик            
+            TimeSpan ts = stopwatch.Elapsed;
+
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", // Создаем строку, содержащую время выполнения операции.
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+
+            l_time.Text = elapsedTime;
+        }
+
+        private void ColoredToBW(string img, byte level = 80)
+        {
+            Bitmap BWBitmap;
+            Color BWColor;
+            using (Bitmap bmp = new Bitmap(img))//присваивание нужно для того что бы можно было передать стринг
+            {
+                BWBitmap = new Bitmap(bmp.Width, bmp.Height);                
+
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color p = bmp.GetPixel(x, y);
+                        bool isWhite = p.R >= level &&
+                                       p.G >= level &&
+                                       p.B >= level;
+
+                        BWColor = isWhite ? Color.White : Color.Black;
+                        BWBitmap.SetPixel(x, y, BWColor);
+                    }
+                }
+            }
+
+            pB_mono.Image = new Bitmap(BWBitmap);
+
+            BWBitmap.Dispose();
+        }
+
+        private void b_next_Click(object sender, EventArgs e)
+        {
+            if ((++fNCount) == fileNames.Length - 1)
+            {
+                b_next.Enabled = false;
+                
+            }
+            else
+            {
+                b_next.Enabled = true;
+            }
+
+            b_prev.Enabled = true;
+            pB_mono.ImageLocation = fileNames[fNCount];
+            l_fileName.Text = safeFileNames[fNCount];
+            ColoredToBW(fileNames[fNCount]);
+        }
+
+        private void b_prev_Click(object sender, EventArgs e)
+        {
+            if (--fNCount == 0)
+            {
+                b_prev.Enabled = false;
+            }
+            else
+            {
+                b_next.Enabled = true;
+                b_prev.Enabled = true;
+            }
+
+            pB_mono.ImageLocation = fileNames[fNCount];
+            l_fileName.Text = safeFileNames[fNCount];
+            ColoredToBW(fileNames[fNCount]);
+        }
+
+        private void cMS_fotPicBox_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if (e.ClickedItem == TSMI_normal)
+            {
+                pB_mono.SizeMode = PictureBoxSizeMode.Normal;
+            }
+            else if (e.ClickedItem == TSMI_stretchImage)
+            {
+                pB_mono.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (e.ClickedItem == TSMI_CenterImage)
+            {
+                pB_mono.SizeMode = PictureBoxSizeMode.CenterImage;
+            }
+            else if (e.ClickedItem == TSMI_zoom)
+            {
+                pB_mono.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            //else if (e.ClickedItem == TSMI_autoSize)
+            //{
+            //    pB_colored.SizeMode = PictureBoxSizeMode.AutoSize;
+            //    pB_mono.SizeMode = PictureBoxSizeMode.AutoSize;
+            //}
+        }
+
+        private void pB_mono_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                //cMS_fotPicBox.Items.Clear(); // Ну, или сразу в OnLoad заполняешь пункты
+                //                contextMenuStrip1.Items.Add("Очистить \"Начислено\"");
+
+                cMS_fotPicBox.Show();
+            }
+        }
+
+        private void NUpD_lavel_ValueChanged(object sender, EventArgs e)
+        {
+            ColoredToBW(pB_mono.ImageLocation, (byte)NUpD_lavel.Value);
+        }
+    }
+}
